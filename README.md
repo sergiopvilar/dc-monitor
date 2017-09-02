b10a5a54205a37588c6419168065f7e21cb2bf95
Sergio Vilar <sergiovilar.r@gmail.com>
2017-09-01 21:54:41 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# DC Monitor
+
+Verifica desconexão em Ragnarok Online usando reconhecimento textual em imagens.
+
+## Pré-requisitos
+
+- Algo que capture screenshots do jogo periodicamente como o ![Auto Screen Capture](https://sourceforge.net/projects/autoscreen/)
+- ![NodeJS](http://nodejs.org)
+- ![Tesseract](https://github.com/tesseract-ocr/tesseract/wiki/Downloads)
+- ![ImageMagick](https://www.imagemagick.org/script/download.php)
+
+## Configurando
+
+- Adicionar Tesseract e ImageMagick ao PATH
+- Fazer com que o software de captura escreva as imagens na pasta `screenshots`
+- Criar um arquivo .env na pasta do projeto da seguinte forma:
+
+```
+TELEGRAM_TOKEN=<SEU_TOKEN>
+```
+
+Sendo `<SEU_TOKEN>` sendo substituído pelo Token do Telegram que você obtém ao ![criar o bot](https://core.telegram.org/bots#creating-a-new-bot).
+
+## Rodando o projeto
+
+Para processar apenas a última screenshot, abra o terminal do NodeJS rode:
+
+    node index.js run
+
+Para iniciar o bot do telegram, abra o terminal do NodeJS rode:
+
+    node index.js bot
+
+## Comandos do bot do telegram
+
+#### /start
+
+Inicia o monitoramento das screenshots
+
+#### /stop
+
+Pausa o monitoramento das screenshots
+
+#### /state
+
+Retorna a última screenshot
+
---
+++ index.js
@@ -0,0 +1,114 @@
+var exec = require('child_process').exec
+  , ncr = require('nodecr')
+  , fs = require('fs')
+  , path = require('path')
+  , _ = require('underscore')
+  , Telegraf = require('telegraf')
+  , yargs = require('yargs')
+
+require('dotenv').config()
+
+class DCMonitor {
+
+  constructor(directory, bot = true) {
+    this.findText = 'Desconectado'
+    this.inputDirectory = directory
+    bot > -1 ? this.run() : this.initBot()
+  }
+
+  run() {
+    this.copyScreenshot()
+    this.processSource(() => this.processImage())
+  }
+
+  warnDC() {
+    this.message('Personagem tomou DC')
+    clearInterval(this.interval)
+    if(!this.ctx) return
+    this.ctx.replyWithPhoto({ source: 'source.jpg' })
+    this.ctx.reply('Parando monitoramento... digite /start para voltar a monitorar.')
+  }
+
+  processImage() {
+    ncr.process(__dirname + '/input.jpg', (err, text) => {
+      if(err) return console.error(err)
+      if(text.indexOf(this.findText) > -1) this.warnDC()
+    }, 'eng', 6)
+  }
+
+  copyScreenshot() {
+    const sourceImg = _.last(this.walkSync(this.inputDirectory))
+    fs.createReadStream(sourceImg).pipe(fs.createWriteStream('source.jpg'));
+  }
+
+  processSource(callback) {
+    exec('convert source.jpg -resize 600% -type Grayscale -gravity center -extent 50% input.jpg', callback);
+  }
+
+  message(message) {
+    console.log(message)
+    if(this.ctx) ctx.reply(message)
+  }
+
+  startMonitor() {
+    clearInterval(this.interval)
+    this.interval = setInterval(this.run, 1 * 60 * 1000)
+    this.message('Monitorando screenshots')
+  }
+
+  stopMonitor() {
+    clearInterval(this.interval)
+    this.message('Parando monitoramento')
+  }
+
+  initBot() {
+    this.app = new Telegraf(process.env.TELEGRAM_TOKEN)
+    this.app.command('/start', (ctx) => {
+      this.ctx = ctx
+      this.startMonitor()
+    })
+
+    app.command('/stop', (ctx) => {
+      this.ctx = ctx
+      this.stopMonitor()
+    })
+
+    app.command('/state', function(ctx) {
+      ctx.reply('Buscando ultima screenshot...')
+      ctx.replyWithPhoto({
+        source: 'source.jpg'
+      })
+    })
+
+    app.startPolling()
+  }
+
+  walkSync(dir, filelist) {
+    let files = fs.readdirSync(dir)
+
+    filelist = filelist || [];
+    files.forEach((file) => {
+      if (fs.statSync(path.join(dir, file)).isDirectory())
+        filelist = this.walkSync(path.join(dir, file), filelist)
+      else
+        filelist.push(path.join(dir, file))
+    })
+    return filelist
+  }
+
+}
+
+const options = (yargs) => {
+  yargs.option('dir', {
+    describe: 'pasta onde as screenshots estão armazenadas',
+    default: 'screenshots'
+  })
+}
+
+yargs.command('run', 'processa a última screenshot', options, (argv) => {
+  new DCMonitor(path.resolve(...argv.dir.split('/')), false)
+}).help().argv
+
+yargs.command('bot', 'liga o bot do telegram', options, (argv) => {
+  new DCMonitor(path.resolve(...argv.dir.split('/')))
+}).help().argv
